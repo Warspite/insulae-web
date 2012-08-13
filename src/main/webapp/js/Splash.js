e724bb4757213cd9864b1f6cf8738147d1614c14
Daniel Franzen <daniel.b.franzen@gmail.com>
2012-08-13 12:29:36 +0200

    Using common 1.5.1. Added splash screen.

---
+++ src/main/webapp/js/Splash.js
@@ -0,0 +1,27 @@
+var Splash = function(targetProgress)
+{
+	mixin(new DynamicNode(), this);
+	this.renderSettings.sizing = {width: Sizing.PARENT, height: Sizing.PARENT};
+	
+	var splashLogo = new DynamicNode();
+	splashLogo.renderSettings.position = {x: 0, y: 0};
+	splashLogo.renderSettings.anchor = {horizontal: Anchor.CENTER, vertical: Anchor.CENTER};
+	splashLogo.renderSettings.origin = {horizontal: Origin.CENTER, vertical: Origin.CENTER};
+	splashLogo.renderSettings.sizing = {width: Sizing.PARENT, height: Sizing.PARENT};
+	splashLogo.renderSettings.sizeRatio = {ratio: 1600/1200, behavior: SizeRatioBehavior.SHRINK};
+	splashLogo.renderSettings.graphicsType = GraphicsType.IMAGE;
+	splashLogo.renderSettings.image = "splash.png";
+	splashLogo.renderSettings.alpha = 0;
+	Animator.spatialAnimate(splashLogo);
+	splashLogo.animationSettings.alphaSpeed = 0.3;
+	splashLogo.spatialSetPaused(false);
+	
+	this.loadingProgress = new ProgressBarNode("Loading . . .");
+	this.loadingProgress.renderSettings.position = {x: 0, y: -20};
+	this.loadingProgress.renderSettings.anchor = {horizontal: Anchor.CENTER, vertical: Anchor.BOTTOM};
+	this.loadingProgress.renderSettings.origin = {horizontal: Origin.CENTER, vertical: Origin.BOTTOM};
+	this.loadingProgress.targetProgress = targetProgress;
+	
+	this.addChild(splashLogo);
+	this.addChild(this.loadingProgress);
+};
--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -14,11 +14,12 @@ function include(file) {
 }
 	
 include("lib/Include.js");
+include("Splash.js");
 
 window.onload = function(event) {
 	var canvas = document.getElementById('mainCanvas');
 	var surface = new RenderSurface(canvas, {x: 25, y: 25});
-	var renderer = new Renderer(surface, "#303030");
+	var renderer = new Renderer(surface, "#ffffff");
 	var mouse = new Mouse(canvas, renderer);
 	var keyboard = new Keyboard(renderer);
 	
@@ -29,4 +30,21 @@ window.onload = function(event) {
 	ticker.addListener(mouse);
 	ticker.addListener(keyboard);
 	ticker.addListener(renderer);
+	
+	var splash = new Splash(20);
+	renderer.guiRoot.addChild(splash);
+	
+	setInterval(function(){ splash.loadingProgress.progress += 1; }, 350);
+	
+//	Session.set({id: 1, key: 123456});
+//	
+//	Server.req("geography/Area", "GET", { "realmId": 1 }, function(result) {
+//		var hej = 0;
+//	});
+//	Server.req("world/Avatar", "GET", { "accountId": 1 }, function(result) {
+//		var hej = 0;
+//	});
+//	Server.req("account/Account", "PUT", { "email": "somethingfunny" }, function(result) {
+//		var hej = 0;
+//	});
 };
